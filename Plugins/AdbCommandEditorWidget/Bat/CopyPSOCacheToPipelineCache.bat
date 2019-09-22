ddeef8c11af73c6467269cbde6c6156f2ec21c2d
pafuhana1213 <pafuhana@hotmail.co.jp>
2019-09-23 00:19:32 +0900

    Update CopyPSOCacheToPipelineCache.bat

--- Plugins/AdbCommandEditorWidget/Bat/CopyPSOCacheToPipelineCache.bat
+++ Plugins/AdbCommandEditorWidget/Bat/CopyPSOCacheToPipelineCache.bat
@@ -2,7 +2,7 @@
 set PROJECT_NAME="ActionRPG"
 set SHADER_FORMAT="GLSL_ES3_1_ANDROID"
 set PROJECT_BUILD_PATH="%~dp0..\..\..\..\Build"
-set PLATFORM_NAME="Android_ASTC"
+set PLATFORM_NAME="Android"
 
 mkdir "%PROJECT_BUILD_PATH%\%PLATFORM_NAME%\PipelineCaches"
 cmd /k copy "%~dp0/%PROJECT_NAME%_%SHADER_FORMAT%.stablepc.csv" "%PROJECT_BUILD_PATH%\%PLATFORM_NAME%\PipelineCaches"
